4f2ca67fc9db51bec97b49761ba8183d17a1c21c
Acoffer <acoffer@foxmail.com>
2021-10-12 14:20:43 +0800

    Add Language

---
+++ In-Objective-C/In-Objective-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  In-Objective-C
+//
+//  Created by Acoffer on 2021/10/12.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ In-Swift/In-Swift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  In-Swift
+//
+//  Created by Acoffer on 2021/10/12.
+//
+
+import Foundation
+
+print("Hello, World!")
+
